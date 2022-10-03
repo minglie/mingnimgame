12393a2e8e05624d79a3fd9bbaf676514076128f
jihefu <1744264136@qq.com>
2022-10-03 18:51:28 +0800

    aa

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -21,16 +21,36 @@ app.use(socketIoWebPlugin,MIO.socketIoWebPluginParam)
 
 const vueConstructorData={
     async mounted() {
-        alert(5)
-        MIO.socketConnect();
+
+        //MIO.socketConnect();
     },
     data() {
         return {
-
+            oneDui:  [1,1,1,0,0,0,0],
+            twoDui:  [1,1,1,1,1,0,0],
+            threeDui:[1,1,1,1,1,1,1],
         }
     },
     methods:{
+        changeVal(dui, value, index){
 
+            console.log(dui, value, index);
+            switch (dui){
+                case 1:{
+                    this.oneDui[index] ^=1;
+                    break;
+                }
+                case 2:{
+                    this.twoDui[index] ^=1;
+                    break;
+                }
+                case 3:{
+                    this.threeDui[index] ^=1;
+                    break;
+                }
+
+            }
+        }
     }
 
 }
